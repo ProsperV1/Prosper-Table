52ad48ed3f3a8592b9a6792f0856d501056d79e6
ProsperV1 <shdjdidabdkd@gmail.com>
2026-06-11 13:48:10 -0600

    Update table.lua

--- table.lua
+++ table.lua
@@ -469,3 +469,4 @@ getgenv()['Prosper'] = {
 		},
 		['No Jump Cooldown'] = true,          
 	}
+     loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/8d6a68350022fba1dd67a0dd07aebaee.lua"))()
