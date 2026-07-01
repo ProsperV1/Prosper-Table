4cd73bd20f7a32aec5ad35ceecfc7fef4ae67bcb
ProsperV1 <shdjdidabdkd@gmail.com>
2026-07-01 17:09:43 -0600

    Update table.lua

--- table.lua
+++ table.lua
@@ -85,7 +85,7 @@ getgenv()['Prosper'] = {
         },
 
         ['Silent Aimbot'] = {
-            ['Enabled'] = false,
+            ['Enabled'] = true,
             ['Hit Point'] = "Surface",  -- Surface // Scaled // Part Name
             ['Scaled'] = { ['Scale'] = 0.1 },
 
@@ -131,7 +131,7 @@ getgenv()['Prosper'] = {
                 ['Enabled'] = true,
                 ['Type'] = 'Simple', -- Simple // Advanced
 
-                ['Simple'] = { {0.104, 0.111} }, -- X // Y
+                ['Simple'] = { {0.124, 0.111} }, -- X // Y
 
                 ['Advanced'] = {
                     ['Strength'] = { {0.24, 0.26} }, -- X // Y 
@@ -148,8 +148,18 @@ getgenv()['Prosper'] = {
             ['Humanize'] = { -- Passes clip checks
                 ['Bezier Curves'] = {
                     ['Enabled'] = false,
-                    ['Curve Intensity'] = 0.5, -- 0 = straight line, 1 = max curve
-                    ['Randomize Direction'] = true -- Randomizes where the curve goes
+                    -- Control Point Count: 1 = one control point, 2 = two control points, 3 = three control points
+                    ['Control Point Count'] = 2,
+                    ['Speed Multiplier'] = 0.7, -- Lower = slower curve (less blatant), 1 = same speed as normal
+                    ['Control Point Offsets'] = { 
+                        -- If your point count = 1 then it will only use your first value, vice versa
+                        -- 0 = no curve, positive = right/up/forward, negative = left/down/backward
+                        -- Bigger number = wider curve
+
+                        ['Offset X'] = { 1.5, -2, 1 }, -- Right / Left
+                        ['Offset Y'] = { 0.8, -1.2, 0.4 }, -- Up / Down
+                        ['Offset Z'] = { -0.5, 1, -0.3 }, -- Forward / Backward (adds depth)
+                    },
                 },
 
                 ['Deadzone Snappiness'] = {
@@ -402,18 +412,18 @@ getgenv()['Prosper'] = {
             ['OutlineColor'] = Color3.fromRGB(0, 0, 0),
 
             ['Box'] = {
-                ['Enabled'] = true,
-                ['Type'] = 'Box' -- Box // Corner (Box is a Bounding Box so use if your fps isnt shit)
+                ['Enabled'] = false,
+                ['Type'] = 'Corner' -- Box // Corner (Box is a Bounding Box so use if your fps isnt shit)
             },
 
             ['Health'] = {
-                ['Enabled'] = true,
-                ['Position'] = 'Left', -- Top // Bottom // Left // Right
+                ['Enabled'] = false,
+                ['Position'] = 'Bottom', -- Top // Bottom // Left // Right
                 ['Mode'] = 'All', -- All // Current Target
             },
 
             ['Distance'] = {
-                ['Enabled'] = true,
+                ['Enabled'] = false,
                 ['Position'] = 'Bottom', -- Top // Bottom // Left // Right
                 ['Font'] = "GothamBold", --[[ https://create.roblox.com/docs/reference/engine/enums/Font ]]
                 ['Size'] = 9,
@@ -428,8 +438,8 @@ getgenv()['Prosper'] = {
             },
 
             ['Armor'] = {
-                ['Enabled'] = true,
-                ['Position'] = 'Left', -- Top // Bottom // Left // Right
+                ['Enabled'] = false,
+                ['Position'] = 'Bottom', -- Top // Bottom // Left // Right
                 ['Mode'] = 'All', -- All // Current Target
             },
 
@@ -442,17 +452,17 @@ getgenv()['Prosper'] = {
         ['Skin Changer'] = { -- changes ur skins (Client-sided)
             ['Enabled'] = true,
             ['Weapons'] = {
-                ['[Revolver]'] = 'Valentine',
-                ['[Double-Barrel SG]'] = 'Valentine',
-                ['[TacticalShotgun]'] = 'Inferno',
-                ['[Knife]'] = 'Red Tiger'
+                ['[Revolver]'] = 'Shiryus Breath',
+                ['[Double-Barrel SG]'] = 'Shiryus Breath',
+                ['[TacticalShotgun]'] = 'Shiryus Breath',
+                ['[Knife]'] = 'Bitcoin'
             },
             ['Beam'] = 'Rainbow' -- Hood Customs Only
         },
 
         ['Wall Jump'] = {
             ['Enabled'] = true,
-            ['Mode'] = 'Double', -- Infinite or Double
+            ['Mode'] = 'Infinite', -- Infinite or Double
             ['Multipliers'] = {
                 ['Regular'] = { ['Multiplier'] = 1.2 },
                 ['Knife'] = { ['Multiplier'] = 1.4 },
@@ -475,7 +485,7 @@ getgenv()['Prosper'] = {
         },
         
         ['Speed Modifications'] = {
-            ['Enabled'] = false,
+            ['Enabled'] = true,
             ['Anti Trip'] = true,
             ['Multipliers'] = {
                 ['Normal'] = { ['Multiplier'] = 35 },
